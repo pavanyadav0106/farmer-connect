7469110161e911d174f36644908a9096ff7400ae
M.Purna Pavan <mangadudlapavan@gmail.com>
2025-06-05 17:22:15 +0530

    Add files via upload

---
+++ js/init.js
@@ -0,0 +1,12 @@
+// Firebase Configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDPrPDAj7o58AijhzBpytGKcsuxHYCGhaY",
+  authDomain: "farmer-connect-8d0ea.firebaseapp.com",
+  projectId: "farmer-connect-8d0ea",
+  storageBucket: "farmer-connect-8d0ea.appspot.com",
+  messagingSenderId: "164327825853",
+  appId: "1:164327825853:web:00c05f326aab3c4683b2d4"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
